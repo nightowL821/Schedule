b86f2e0cb3e5332f905d263bece0f78332ac816b
nightowL821 <101846848+nightowL821@users.noreply.github.com>
2024-11-13 19:37:08 +0600

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-function checkName() {
-
-    var name = document.getElementById("name").value.trim().toLowerCase();
-
-    var validNames = [
-        "mrittika", "mrittike", "tanbin rubayra mrittika", 
-        "tanbin rubayra mrittika", "babesh"
-    ];
-    if (validNames.includes(name)) {
-        window.location.href = "nextQuestionPage.html"; 
+function checkAnswer() {
+    var answer1 = document.getElementById("answer1").value.trim().toLowerCase();
+    
+    if (answer1 === "mrittika" || answer1 === "mrittike" || answer1 === "tanbin rubayra mrittika" || answer1 === "tanbin rubayra mrittika" || answer1 === "babesh") {
+        window.location.href = "question2.html"; 
     } else {
-        window.location.href = "notGirlfriend.html";
+        window.location.href = "notGirlfriend.html"; 
     }
 }
